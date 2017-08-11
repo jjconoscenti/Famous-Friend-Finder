b170c0aaf8ee471e597d47dab3e056ce295be5d8
Giuseppe Conoscenti <jjconoscenti@Giuseppes-MacBook-Pro.local>
2017-08-10 22:42:05 -0500

    folder structure change

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
 var bodyParser = require('body-parser');
 var path = require('path');
 var express = require('express');
+
+// add public path
+
+// confugure express to serve survey and home files (serve entire public folder)
