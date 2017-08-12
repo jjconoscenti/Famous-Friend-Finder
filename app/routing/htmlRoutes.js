211d78a93b87ffc78845fdd260be38a1590c4256
Giuseppe Conoscenti <jjconoscenti@Giuseppes-MacBook-Pro.local>
2017-08-12 12:18:05 -0500

    adds

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,4 @@
+// A GET Route to /survey which should display the survey page.
+
+
+// A default USE route that leads to home.html which displays the home page.
--- server.js
+++ server.js
@@ -3,6 +3,10 @@ var path = require('path');
 var express = require('express');
 
 
+
+
 // add public path
 
 // confugure express to serve survey and home files (serve entire public folder)
+
+app.get();
