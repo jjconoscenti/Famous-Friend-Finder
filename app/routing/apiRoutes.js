abc94872c07a2bbae9aafe7e2ab24d9e0b34538c
Giuseppe Conoscenti <jjconoscenti@Giuseppes-MacBook-Pro.local>
2017-08-15 22:27:02 -0500

    api routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,4 +1,14 @@
-// A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
+var friendListData = require('../data/friends');
 
-
-// A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+module.exports = function(app) {
+    // A GET route with the url /api/friends. This will be used to display a JSON of all possible friends
+    app.get('/api/friends', function(req, res) {
+        res.json(friendListData);
+    });
+    // A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+    app.post('/api/friends', function(req, res) {
+        friendListData.push(req.body);
+        res.json(true);
+        res.direct('/');
+    });
+};
