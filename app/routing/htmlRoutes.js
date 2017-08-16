1d2ac3a85a7e40bd618fb093f857ed3c99a3ad1c
Giuseppe Conoscenti <jjconoscenti@Giuseppes-MacBook-Pro.local>
2017-08-15 22:38:01 -0500

    html routes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,4 +1,12 @@
-// A GET Route to /survey which should display the survey page.
+var path = require('path');
 
-
-// A default USE route that leads to home.html which displays the home page.
+module.exports = function(app) {
+    // A GET Route to /survey which should display the survey page.
+    app.get('/survey', function(req, res) {
+        res.sendFile(path.join(__dirname, "/../public/survey.html"));
+    });
+    // A default USE route that leads to home.html which displays the home page.
+    app.get('/', function(req, res) {
+        res.sendFile(path.join(__dirname, '/../public/home.html'));
+    });
+};
