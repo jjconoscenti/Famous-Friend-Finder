e8329fd338e6169bcb0d12c5eba37fcfd336c950
Giuseppe Conoscenti <jjconoscenti@Giuseppes-MacBook-Pro.local>
2017-08-18 20:41:19 -0500

    updated friends file

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,7 +2,7 @@ var friendsList = [{
         name: "Matt Damon",
         twitter: "@mattdamon",
         instagram: "@mattdamon",
-        photo: 'mattdamon.jpg',
+        photo: 'http://images.boomsbeat.com/data/images/full/50403/matt_damon_66-me_festival_de_venise_-mostra-jpg.jpg',
         scores: [
             '2',
             '4',
@@ -17,10 +17,10 @@ var friendsList = [{
         ]
     },
     {
-        name: "Jennifer Lawrence",
-        photo: 'jenniferlawrence.jpg',
-        twitter: "@jlaw",
-        instagram: "@jlaw",
+        name: "Ben Affleck",
+        twitter: "@benaffleck",
+        instagram: "@benaffleck",
+        photo: 'http://akns-images.eonline.com/eol_images/Entire_Site/2017214/rs_1024x759-170314154453-1024-ben-affleck2.jpg',
         scores: [
             '5',
             '4',
@@ -38,7 +38,7 @@ var friendsList = [{
         name: "Oprah Winfrey",
         twitter: "@oprahwinfrey",
         instagram: "@oprah",
-        photo: "oprah.jpg",
+        photo: "http://thinklink.in/wp-content/uploads/oprahwinfrey.jpg",
         scores: [
             '1',
             '2',
@@ -56,7 +56,7 @@ var friendsList = [{
         name: "Jennifer Lopez",
         twitter: "@jenniferlopez",
         instagram: "@jlo",
-        photo: "jenniferlopez.jpg",
+        photo: "https://www.biography.com/.image/t_share/MTE4MDAzNDEwNzQ4NjA1OTY2/jennifer-lopez-9542231-3-402.jpg",
         scores: [
             '5',
             '4',
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,6 +9,6 @@ module.exports = function(app) {
     app.post('/api/friends', function(req, res) {
         friendListData.push(req.body);
         res.json(true);
-        res.direct('/');
+        res.redirect('/');
     });
 };
